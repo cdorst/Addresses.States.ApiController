8a0333c92677a804e45f8516139eda87f321a582
Christopher Dorst <chris@dorst.email>
2018-03-29 12:17:54 -0500

    Update StatesController.cs

--- Addresses.States.ApiController/StatesController.cs
+++ Addresses.States.ApiController/StatesController.cs
@@ -4,6 +4,7 @@
 using Addresses.States.DatabaseContext;
 using DevOps.Code.DataAccess.Interfaces.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System.Threading.Tasks;
 
 namespace Addresses.States.ApiController
